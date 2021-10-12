972f5603516abb5047c073ad4652b342a3da9d8e
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-10-12 14:08:32 +0200

    Add person, corrector, student

---
+++ corrector.rb
@@ -0,0 +1,7 @@
+# corrector class
+class Corrector
+  def correct_name(name)
+    name.capitalize!
+    name[0, 10]
+  end
+end
---
+++ person.rb
@@ -0,0 +1,32 @@
+require './corrector'
+require './rentals'
+
+# persons class
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @corrector = Corrector.new
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    @parent_permission || of_age?
+  end
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  has_many :rentals
+end
